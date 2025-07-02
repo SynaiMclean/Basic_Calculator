e8fbfff5bc8c1f0c4970873e66e730a90fb851d8
Synai <41596900+SynaiMclean@users.noreply.github.com>
2025-07-01 22:13:43 -0400

    Testing button functionality

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const display = document.querySelector(".bar");
 let displayValue = "";
+let firstNumber = "";
+let operation = "";
+let secondNumber = "";
+let waitingForOperand = false;
 
 const buttons = document.querySelectorAll(".button");
 for(let i=0; i < buttons.length; i++){
@@ -7,7 +11,9 @@ for(let i=0; i < buttons.length; i++){
    const buttonText = event.target.textContent;
    displayValue += buttonText
    display.textContent = displayValue;
+   
     });
+
 }
 
 const operationButtons = document.querySelectorAll(".button-s");
@@ -15,10 +21,36 @@ const operationButtons = document.querySelectorAll(".button-s");
 for(let i = 0; i < operationButtons.length; i ++){
     operationButtons[i].addEventListener('click', function(event){
         const buttonText = event.target.textContent;
-        displayValue += buttonText;
-        display.textContent = displayValue;
 
-    });
-    };
+        if (buttonText === "A/C"){
+            displayValue = "";
+            firstNumber = "";
+             operation = "";
+            secondNumber = "";
+            display.textContent
+
+        }
+    
+
+        })
+        }
+
+    
 
 
+ // displayValue += buttonText;
+        // display.textContent = displayValue;
+
+        // if(buttonText === "+"){
+        //    displayValue = firstNumber + secondNumber;
+        // }else if(buttonText === "-"){
+        //     displayValue = firstNumber - secondNumber;
+        // }else if(buttonText === "X" ){
+        //     displayValue = firstNumber * secondNumber;
+        // }else if(buttonText === "/"){
+        //     displayValue = firstNumber / secondNumber;
+        // }else if( buttonText === "A/C"){
+        //     displayValue = ""
+        // }else if(buttonText === "="){
+        //     display.textContent = displayValue
+        // }
